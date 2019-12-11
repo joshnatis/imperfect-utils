2fbced67bc42b059f00f729eef5400661e4200d4
Josh Natis <joshnatis0@gmail.com>
2019-12-11 06:33:50 -0500

    Fix bugs and add functionality

--- games/snake-golf.cpp
+++ games/snake-golf.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 /* Global Variables */
-const int ROWS = 40; 
-const int COLS = 40;
+const int ROWS = 20; 
+const int COLS = 50;
 char MAP[ROWS][COLS];
 
 int VISIBLE_ROWS;
@@ -15,7 +15,9 @@ int VISIBLE_COLS;
 int snake_col = 0;
 int snake_row = 0;
 
-int points = 0;
+int points = 0; //total apples found
+int avg = -1; //average moves to get apple throughout session
+int moves = 0; //total user cued moves (e.g wasd is 4 moves)
 
 int apple_row = 0;
 int apple_col = 0;
@@ -26,7 +28,7 @@ void initialize();
 void spawnApple();
 void printMap();
 void moveSnake(char dir);
-char getUserInput();
+string getUserInput();
 void printBorder(char pos);
 
 int main()
@@ -37,9 +39,13 @@ int main()
 	{
 		spawnApple();
 		printMap();
-		char dir = getUserInput();
-		moveSnake(dir);
+		string dir = getUserInput();
+		for(int i = 0, size = dir.length(); i < size; ++i)
+		{
+			moveSnake(dir[i]);
+		}
 		clearScreen();
+		++moves; //every combination of letters is a move, not every letter
 	}
 	return 0;
 }
@@ -51,13 +57,13 @@ void clearScreen()
 }
 void spawnApple()
 {
-	if(snake_row == apple_row && snake_col == apple_col)
+	while(snake_row == apple_row && snake_col == apple_col)
 	{
 		apple_row = rand() % VISIBLE_ROWS;
 		apple_col = rand() % VISIBLE_COLS;
-	
-		MAP[apple_row][apple_col] = 'O';
 	}
+	//apple location guaranteed not to be snake location
+	MAP[apple_row][apple_col] = 'O';
 }
 
 void printBorder(char pos)
@@ -73,9 +79,9 @@ void printBorder(char pos)
 	cout << "\n";
 }
 
-char getUserInput()
+string getUserInput()
 {
-	char input;
+	string input;
 	cin >> input;
 	return input;
 }
@@ -92,34 +98,45 @@ void moveSnake(char dir)
 	else if(dir == 's')
 	{
 		MAP[snake_row][snake_col] = ' ';
-                MAP[snake_row + 1][snake_col] = '*';
-                snake_row++;
+		MAP[snake_row + 1][snake_col] = '*';
+		snake_row++;
 	}
 
 	else if(dir == 'a')
 	{
 		MAP[snake_row][snake_col] = ' ';
-                MAP[snake_row][snake_col - 1] = '*';
-                snake_col--;
+		MAP[snake_row][snake_col - 1] = '*';
+		snake_col--;
 	}
 	
 	else if(dir == 'd')
 	{
 		MAP[snake_row][snake_col] = ' ';
-                MAP[snake_row][snake_col + 1] = '*';
-                snake_col++;
+		MAP[snake_row][snake_col + 1] = '*';
+       	snake_col++;
 	}
 
+	//found apple
 	if(snake_row == apple_row && snake_col == apple_col)
 	{
+		if(points == 0) //first turn
+			avg = moves;
+		else
+			avg = (avg + moves)/2; //average previous avg and current avg
+		
+		//reset state counters
+		moves = 0;
+
 		points++;
-		MAP[apple_row][apple_col] == ' ';	
+
+		MAP[apple_row][apple_col] = ' ';
+		MAP[apple_row][apple_col] = '*';
 	}
 }
 
 void printMap()
 {
-	cout << "POINTS: " << points << endl;
+	cout << "AVERAGE MOVES: " << avg << " | MOVE #" << moves << " | POINTS: " << points << endl;
 	printBorder('t');
 	for(int i = 0; i < VISIBLE_ROWS; ++i)
 	{
@@ -137,8 +154,8 @@ void printMap()
 void initialize()
 {
 	srand (time(NULL));
-	VISIBLE_ROWS = 1 + rand() % (ROWS - 1);
-	VISIBLE_COLS = 1 + rand() % (COLS - 1);
+	VISIBLE_ROWS = 1 + (rand() % (ROWS - 1));
+	VISIBLE_COLS = 1 + (rand() % (COLS - 1));
 	
 	clearScreen();
 
